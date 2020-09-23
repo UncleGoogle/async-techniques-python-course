b5ecc465f29cf3442d1f87ddfe42a82036dd94ca
UncleGoogle <mieszkoziemowit@gmail.com>
2020-09-23 21:36:14 +0200

    shorter ver

--- src/04-asyncio/producer_consumer/prod_async/async_program.py
+++ src/04-asyncio/producer_consumer/prod_async/async_program.py
@@ -1,6 +1,5 @@
 import asyncio
-import datetime
-import colorama
+import time
 import random
 
 
@@ -8,28 +7,28 @@ def main():
 
     loop = asyncio.get_event_loop()
 
-    t0 = datetime.datetime.now()
-    print(colorama.Fore.WHITE + "App started.", flush=True)
+    t0 = time.time()
+    print("| App started.", flush=True)
 
     data = asyncio.Queue()
 
-    task1 = loop.create_task(generate_data(20, data))
-    task2 = loop.create_task(generate_data(20, data))
+    task1 = loop.create_task(generate_data(10, data))
+    task2 = loop.create_task(generate_data(8, data))
     task3 = loop.create_task(process_data(40, data))
 
     final_task = asyncio.gather(task1, task2, task3)
     loop.run_until_complete(final_task)
 
-    dt = datetime.datetime.now() - t0
-    print(colorama.Fore.WHITE + "App exiting, total time: {:,.2f} sec.".format(dt.total_seconds()), flush=True)
+    dt = time.time() - t0
+    print("| App exiting, total time: {:,.2f} sec.".format(dt), flush=True)
 
 
 async def generate_data(num: int, data: asyncio.Queue):
     for idx in range(1, num + 1):
         item = idx*idx
-        await data.put((item, datetime.datetime.now()))
+        await data.put((item, time.time()))
 
-        print(colorama.Fore.YELLOW + " -- generated item {}".format(idx), flush=True)
+        print("-->> Generated item {}".format(idx), flush=True)
         await asyncio.sleep(random.random() + .5)
 
 
@@ -39,12 +38,10 @@ async def process_data(num: int, data: asyncio.Queue):
         item = await data.get()
 
         processed += 1
-        value = item[0]
-        t = item[1]
-        dt = datetime.datetime.now() - t
+        value, t = item
+        dt = time.time() - t
 
-        print(colorama.Fore.CYAN +
-              " +++ Processed value {} after {:,.2f} sec.".format(value, dt.total_seconds()), flush=True)
+        print("<< Processed value {} after {:,.2f} sec.".format(value, dt), flush=True)
         await asyncio.sleep(.5)
 
 
